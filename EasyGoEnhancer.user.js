e97d0ff9afdb4e7763c61b1da3a3e22b82752203
creamidea <creamidea@gmail.com>
2016-11-17 13:16:32 +0800

    fix(load): load all todos

--- EasyGoEnhancer.user.js
+++ EasyGoEnhancer.user.js
@@ -13,31 +13,53 @@
 // ==/UserScript==
 
 (function() {
-	'use strict';
+  'use strict';
 
-	//定义一个数组：待办事项的类型：
-	var ToDoListName = ["业务项目","业务报告","人文财务","独立性","综合"];
+  //定义一个数组：待办事项的类型：
+  var toDoListName = ["业务项目", "业务报告", "人文财务", "独立性", "综合"];
 
-	// 删除首页中“待办事项”后面的伪Tags：
-	var $FakeTags = $(".NewTitle1");
+  // 删除首页中“待办事项”后面的伪Tags：
+  var $FakeTags = $(".NewTitle1");
 
-	// 装载数据的容器
-	var $container = $('<div />').html('加载数据中，请稍后...');
+  // 装载数据的容器
+  var $hint = $('<span />').html('加载数据中，请稍后...');
+  var $container = $('<div />');
 
-	// 数据请求源
-	var PerUrl = "http://www.ascendacpa.com.cn/MoreTask3.aspx?ind=1 " + "[id*=tabContent__]>div";
+  // 数据请求源
+  var url = "http://www.ascendacpa.com.cn/MoreTask3.aspx";
 
-	// tampermonkey 在运行的时候，碰到页面有 frame 标签，会对其每一个运行脚本
-	// 这里排除没有找到相关元素的 frame
-	if (!$FakeTags.length === 0) return;
+  // tampermonkey 在运行的时候，碰到页面有 frame 标签，会对其每一个运行脚本
+  // 这里排除没有找到相关元素的 frame
+  if ($FakeTags.length === 0) return;
 
-	// 移除原先的 业务项目 等导航
-	$FakeTags.children().not(':first').remove();
+  // 移除原先的 业务项目 等导航
+  $FakeTags.children().not(':first').remove();
 
-	// 将容器插入 DOM
-	$container.insertAfter($FakeTags);
+  // 将容器插入 DOM
+  $container.insertAfter($FakeTags);
 
-	// 装载数据
-	$container.load(PerUrl);
+  // 请求每一项的 ToDo
+  for (var i = 0; i < 5; i++) {
+    var name = toDoListName[i];
+
+    // 构建包裹元素
+    var $elt = $('<table/>')
+      .attr('data-name', name)
+      .addClass('tablebox')
+      .css({
+        'text-align': 'left'
+      })
+      .html('<span>加载<strong>' + name + '</strong>中，请稍后...</span>');
+    $container.append($elt);
+
+    // 加载数据
+    $elt.load(url + " .table-news tbody", {
+      __EVENTTARGET: 'ctl00$PageBody$TabOptionItem' + (i + 1)
+    }, function(response, status, xhr) {
+      if (status === 'error') {
+        $(this).html('<span style="color: red;">加载<strong>' + $(this).attr('data-name') + '</strong>失败</span>');
+      }
+    });
+  }
 
 })();
