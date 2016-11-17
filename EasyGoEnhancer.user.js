ff377ade9cac99cf8eb08590e46e8a8d270c0dac
maoger <maoyantsing@gmail.com>
2016-11-17 14:56:02 +0800

    V 1.4
    修复后续子tags的待办事项无法load的bug。

--- EasyGoEnhancer.user.js
+++ EasyGoEnhancer.user.js
@@ -2,7 +2,7 @@
 // @name         EasyGoEnhancer
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/maoger/EasyGoEnhancer
-// @version      1.3.1
+// @version      1.4
 // @description  首页显示EasyGo待办事项
 // @author       Maoger
 // @match        http://www.ascendacpa.com.cn/*
@@ -12,50 +12,51 @@
 // ==/UserScript==
 
 (function() {
-	'use strict';
+    'use strict';
 
-	//定义一个数组：待办事项的类型：
-	var ToDoListName = ["业务项目","业务报告","人文财务","独立性","综合"];
+    // 待办事项所在子网页
+    var url = "http://www.ascendacpa.com.cn/MoreTask3.aspx";
 
-	// 删除首页中“待办事项”后面的伪Tags：
-	var FakeTags = document.querySelector(".NewTitle1");
-	for (var i = 5 - 1; i >= 0; i--) {
-		FakeTags.removeChild(FakeTags.children[1]);
-	}
+    //定义一个数组：待办事项的类型
+    var ToDoListName = ["业务项目","业务报告","人文财务","独立性","综合"];
 
-	// 增加容器，并插入到“待办事项”标题下:
-	// 以下为为了使用 insertBefore 而进行定位查找相关元素：
-	var MaoList = document.querySelector(".contPage");
-	var MaoTitle = document.querySelector(".newfooter");
+    // 定位：首页中“待办事项”栏的伪Tags
+    var $FakeTags = $(".NewTitle1");
 
-	for (var m = 0; m <= 5-1; m++) {
-		// 新建一个div容器：
-		var ToDoList = document.createElement('div');
-		var PerId ="ToDoList" + m.toString();
-		ToDoList.id = PerId;
+    // 新建装载数据的容器
+    var $hint = $('<span />').html('加载数据中，请稍后...');
+    var $container = $('<div />');
 
+    // tampermonkey 在运行的时候，碰到页面有 frame 标签，会对其每一个运行脚本
+    // 这里排除没有找到相关元素的 frame
+    if ($FakeTags.length === 0) return;
 
-		// 把div容器插入到大标题“待办事项”下：
-		MaoList.insertBefore(ToDoList, MaoTitle);
+    // 移除原先的 业务项目 等导航
+    $FakeTags.children().not(':first').remove();
 
-		// 新建一个table容器：
-		var MaoTable = document.createElement('table');
-		MaoTable.className = "tablebox";
-		MaoTable.border = "0";
-		MaoTable.style = "border-collapse:collapse";
-		var MaoTbody = document.createElement("tbody");
-		MaoTbody.id = "ToDoListTable" + m.toString();
-		MaoTbody.textContent = ToDoListName[m] + "is downloading ......";
+    // 将容器插入 DOM
+    $container.insertAfter($FakeTags);
 
-		// 在div容器内插入table空白表格用于格式化显示“待办事项”
-		ToDoList.appendChild(MaoTable);
-		MaoTable.appendChild(MaoTbody);
+    // 请求并获取每一项的 ToDo
+    for (var i = 0; i < 5; i++) {
+    var name = ToDoListName[i];
 
+    // 构建包裹元素
+    var $elt = $('<table/>')
+        .attr('data-name', name)
+        .addClass('tablebox')
+        .css({
+        'text-align': 'left'
+        })
+        .html('<span>正在加载<b>' + name + '</b>中，请稍后...</span>');
+    $container.append($elt);
 
-
-		// 使用load()方法获取待办事项，并插入div容器中
-		var JPerId = "#" + "ToDoListTable" + m.toString();
-		var PerUrl = "http://www.ascendacpa.com.cn/MoreTask3.aspx " + "#tabContent__" + m.toString() + " div:first tr:gt(0)";
-		$(JPerId).load(PerUrl);
-	}
+    // 加载数据
+    var PerUrl = "http://www.ascendacpa.com.cn/MoreTask3.aspx?ind=" + (i+1).toString() + " " + "#tabContent__" + i.toString() + " " + "div:first tr:gt(0)";
+    $elt.load(PerUrl, function(response, status, xhr) {
+        if (status === 'error') {
+            $(this).html('<span style="color: red;">加载<b>' + $(this).attr('data-name') + '</b>失败</span>');
+        }
+    });
+    }
 })();
