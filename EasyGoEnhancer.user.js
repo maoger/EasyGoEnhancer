ddc909bcc5171676944204a5352a9cb79611bb2f
maoger <maoyantsing@gmail.com>
2016-11-18 09:32:26 +0800

    V 1.5.2
    merge之后调整了一下格式

--- EasyGoEnhancer.user.js
+++ EasyGoEnhancer.user.js
@@ -2,7 +2,7 @@
 // @name         EasyGoEnhancer
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/maoger/EasyGoEnhancer
-// @version      1.5.1
+// @version      1.5.2
 // @description  首页显示EasyGo待办事项
 // @author       Maoger
 // @match        http://www.ascendacpa.com.cn/*
@@ -24,14 +24,14 @@
     var $FakeTags = $(".NewTitle1");
 
     // 新建:装载数据的容器
-    var $container = $('<div />');
+    var $container = $("<div />");
 
     // tampermonkey 在运行的时候，碰到页面有 frame 标签，会对其每一个运行脚本
     // 这里排除没有找到相关元素的 frame
     if ($FakeTags.length === 0) return;
 
     // 移除:首页中“待办事项”栏的伪Tags
-    $FakeTags.children().not(':first').remove();
+    $FakeTags.children().not(":first").remove();
 
     // 将容器插入 DOM
     $container.insertAfter($FakeTags);
@@ -41,20 +41,20 @@
     var name = ToDoListName[i];
 
     // 构建包裹元素
-    var $elt = $('<table/>')
-        .attr('data-name', name)
-        .addClass('tablebox')
+    var $elt = $("<table/>")
+        .attr("data-name", name)
+        .addClass("tablebox")
         .css({
-        'text-align': 'left'
+        "text-align": "left"
         })
-        .html('<span>正在加载<b>' + name + '</b>中，请稍候...</span>');
+        .html("<span>正在加载<b>" + name + "</b>中，请稍候...</span>");
     $container.append($elt);
 
     // 加载数据
     var PerUrl = "http://www.ascendacpa.com.cn/MoreTask3.aspx?ind=" + (i+1).toString() + " " + "#tabContent__" + i.toString() + " " + "div:first tr:gt(0)";
     $elt.load(PerUrl, function(response, status, xhr) {
-        if (status === 'error') {
-            $(this).html('<span style="color: red;">加载<b>' + $(this).attr('data-name') + '</b>失败！</span>');
+        if (status === "error") {
+            $(this).html('<span style="color: red;">加载<b>' + $(this).attr("data-name") + '</b>失败！</span>');
         }
     });
     }
