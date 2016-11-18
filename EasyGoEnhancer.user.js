1e819b2e5e470a0c73852b6eea382b5fc5ff70fd
maoger <maoyantsing@gmail.com>
2016-11-18 21:02:15 +0800

    V 2.0
    添加进度条UI。

--- EasyGoEnhancer.user.js
+++ EasyGoEnhancer.user.js
@@ -2,7 +2,7 @@
 // @name         EasyGoEnhancer
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/maoger/EasyGoEnhancer
-// @version      1.5.4
+// @version      2.0
 // @description  重整EasyGo待办事项，直接首页呈现明细。
 // @author       Maoger
 // @match        http://www.ascendacpa.com.cn/*
@@ -36,27 +36,75 @@
     // 将容器插入 DOM
     $container.insertAfter($FakeTags);
 
+    // 新建：存放进度条的容器
+    var $loadContainer = $("<div/>")
+        .addClass("barSpace")
+        .css({
+            "margin" : "1px 0px",
+            "background" : "grey",
+            "border-radius" : "1px",
+        });
+
+    // 将进度条的容器插入 DOM
+    $loadContainer.insertAfter($FakeTags);
+
+    // 新建：进度条
+    var $bar = $("<span/>")
+        .attr("id" , "barsmooth" )
+        .addClass("smooth")
+        .css({
+            "height" : "1px",
+            "width" : "0%",
+            "border-radius" : "1px",
+            "display" : "block",
+            "box-shadow" : "0px 0px 10px 1px #3B8CF8",
+            "background-color" : "#fff"
+        });
+
+    // 将 进度条 插入 DOM
+    $loadContainer.append($bar);
+
+    // 定义：一个Number类型变量，用于设置进度条的width的百分比
+    var m = 0;
+
     // 请求并获取每一项的 ToDo
     for (var i = 0, max = ToDoListName.length; i < max; i++) {
-    var name = ToDoListName[i];
+        var name = ToDoListName[i];
 
-    // 构建包裹元素
-    var $elt = $("<table/>")
+        // 构建包裹元素
+        var $elt = $("<table/>")
         .attr("data-name", name)
         .addClass("tablebox")
         .css({
             "text-align": "left"
         })
         .html("<span>正在加载<b>" + name + "</b>中，请稍候...</span>");
-    $container.append($elt);
+        $container.append($elt);
 
-    // 加载数据
-    var PerUrl = "http://www.ascendacpa.com.cn/MoreTask3.aspx?ind=" + (i+1).toString() + " " + "#tabContent__" + i.toString() + " " + "div:first tr:gt(0)";
-    $elt.load(PerUrl, function(response, status, xhr) {
-        if (status === "error") {
-            $(this).html('<span style="color: red;">加载<b>' + $(this).attr("data-name") + '</b>失败！</span>');
-        }
-    });
+        // 从服务器上“待办事项”所在位置加载数据
+        var PerUrl = "http://www.ascendacpa.com.cn/MoreTask3.aspx?ind=" + (i+1).toString() + " " + "#tabContent__" + i.toString() + " " + "div:first tr:gt(0)";
+
+        $elt.load(PerUrl, function(responseTxt , statusTxt , xhr) {
+            if (statusTxt === "success") {
+                // 设置进度条显示的百分比
+                var loadingRate = self.setInterval(function (){
+                    if ( m < (i-1)*20 ) {
+                        m = (i-1)*20;
+                        $bar.css("width",m.toString() + "%");
+                    }
+                    else if( m < i*20 ){
+                        m = m + 0.1;
+                        $bar.css("width",m.toString() + "%");
+                    }
+                    if ( m >= 100 ) {
+                        $bar.css("width" , "100%");
+                        clearInterval(loadingRate);
+                    }
+                },10);
+            }
+            if (statusTxt === "error") {
+                $(this).html('<span style="color: red;">加载<b>' + $(this).attr("data-name") + '</b>失败！</span>');
+            }
+        });
     }
-
 })();
