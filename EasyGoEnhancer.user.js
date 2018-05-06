979aac3a74d3e10d3800259d28c406a53ef975f4
Maoger <maoyantsing@gmail.com>
2018-05-06 21:39:48 +0800

    usual update

--- EasyGoEnhancer.user.js
+++ EasyGoEnhancer.user.js
@@ -2,7 +2,7 @@
 // @name         EasyGoEnhancer
 // @namespace    http://tampermonkey.net/
 // @homepage     https://github.com/maoger/EasyGoEnhancer
-// @version      2.8.18022701
+// @version      2.8.180506
 // @description  更加效率的EasyGo.
 // @author       Maoger
 // @match        http://*.ascendacpa.com.cn/*
@@ -20,7 +20,7 @@ function Load_ToDoList() {
     'use strict';
 
     // trick：修改网页的title
-    $("title").html("EasyGo | Maoger | 祝大家年审快乐");
+    $("title").html("EasyGo | Maoger | 年审结束了 该收收心 准备好好休息了");
 
     // 定义：“待办事项”所在子网页
     var DBSX_url = "/MoreTask3.aspx";
